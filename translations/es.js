23ff8f00a2760670ba2818518bd7ebedd0a9d664
rouif <rouif23@gmail.com>
2026-08-06 19:03:32 +0100

    Update es.js

--- translations/es.js
+++ translations/es.js
@@ -21,4 +21,11 @@ menuExperience: "Experiencia",
 menuGallery: "Galería",
 menuCertificates: "Certificados",
 menuContact: "Contacto"
+welcomeText: "Bienvenido a mi portafolio profesional. Aquí puedes conocer mi experiencia sanitaria, educación, certificados y trabajo humanitario.",
+
+storyText: "Soy auxiliar de enfermería con experiencia en atención al paciente, apoyo sanitario y misiones humanitarias.",
+
+journeyText: "Mi trayectoria profesional incluye asistencia sanitaria, campañas médicas y aprendizaje continuo.",
+
+contactText: "Estoy abierto a oportunidades profesionales, colaboraciones y proyectos sanitarios."
 };
