2cc353af795d794ab91d7c7afaffdd94c8cf2dfc
rouif <rouif23@gmail.com>
2026-08-06 18:45:16 +0100

    Update fr.js

--- translations/translations/fr.js
+++ translations/translations/fr.js
@@ -1,19 +1,1 @@
-const fr = {
 
-    name: "ANAS ROUIF",
-
-    title: "Professionnel de santé",
-
-    welcome: "Bienvenue dans mon parcours professionnel",
-
-    story: "Mon histoire",
-
-    journey: "Parcours professionnel",
-
-    gallery: "Galerie",
-
-    certificates: "Certificats et formations",
-
-    contact: "Contactez-moi"
-
-};
