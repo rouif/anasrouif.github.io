44805b2d4303e51e0aedf022d318c51631b4eff5
rouif <rouif23@gmail.com>
2026-08-06 19:08:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,17 +22,19 @@ function changeLanguage(language) {
     document.querySelector("h1").textContent = text.name;
 
     document.querySelector(".hero h2").textContent = text.welcome;
-
+document.querySelector(".hero p").textContent = text.welcomeText;
 
     document.querySelector("#story h2").textContent = text.story;
-
+document.querySelector("#story p").textContent = text.storyText;
     document.querySelector("#journey h2").textContent = text.journey;
-
+document.querySelector("#journey > p").textContent = text.journeyText;
     document.querySelector("#gallery h2").textContent = text.gallery;
 
     document.querySelector("#certificates h2").textContent = text.certificates;
 
     document.querySelector("#contact h2").textContent = text.contact;
+    document.querySelector("#contact > p").textContent = text.contactText;
+    
 document.querySelector("nav a:nth-child(1)").textContent = text.menuHome;
 
 document.querySelector("nav a:nth-child(2)").textContent = text.menuAbout;
